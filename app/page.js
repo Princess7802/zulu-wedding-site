95709870d91ccb88a5066fe9d1ee59388781f728
Princess7802 <154804614+Princess7802@users.noreply.github.com>
2026-06-29 18:45:46 +0200

    update website

--- app/page.js
+++ app/page.js
@@ -673,7 +673,7 @@ export default function ZuluHeritageWeddingInvitation() {
                     <h3
                       style={{
                         color: "#f5e6c8",
-                        fontSize: "32px",
+                        fontSize: "30px",
                         margin: "0 0 2px",
                         fontFamily: "'Cormorant Garamond', Georgia, serif",
                         fontWeight: "400",
@@ -759,7 +759,7 @@ export default function ZuluHeritageWeddingInvitation() {
                     <h3
                       style={{
                         color: "#f5e6c8",
-                        fontSize: "32px",
+                        fontSize: "30px",
                         margin: "0 0 2px",
                         fontFamily: "'Cormorant Garamond', Georgia, serif",
                         fontWeight: "400",
